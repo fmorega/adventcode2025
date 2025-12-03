80bb5cebe7e2e61aad98810d8b3aa6d6d5008fed
Francisco Jose Moreno Gabaldon <fmorega@mercadona.es>
2025-12-03 12:35:16 +0100

    feat: 2025E03 solved part two

--- src/main/java/com/fran/practice/adventcode2025/E03_EscalatorBatteries.java
+++ src/main/java/com/fran/practice/adventcode2025/E03_EscalatorBatteries.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class E03_EscalatorBatteries implements Exercise {
 
   private static final String INPUT_DAY_THREE = "inputs/day03-2025.txt";
+  private static final int PART2_DIGITS = 12;
+
   @Override
   public String id() {
     return "AOC25-03";
@@ -31,7 +33,10 @@ public class E03_EscalatorBatteries implements Exercise {
       List<String> banks = parseBanks(rawInput);
 
       long totalJoltage = solvePartOne(banks);
+      long totalPart2 = solvePartTwo(banks);
+
       System.out.println("Part 1 - Total output joltage: " + totalJoltage);
+      System.out.println("Part 2 - Total output joltage: " + totalPart2);
     } catch (IOException e) {
       System.out.println("Error reading file: " + e.getMessage());
     }
@@ -51,13 +56,25 @@ public class E03_EscalatorBatteries implements Exercise {
     long total = 0L;
 
     for (String bank : banks) {
-      int maxForBank = maxJoltageForBank(bank);
+      int maxForBank = maxJoltageForBankTwoDigits(bank);
       total += maxForBank;
     }
     return total;
   }
 
-  static int maxJoltageForBank(String bank) {
+  static long solvePartTwo(List<String> banks) {
+    long total = 0L;
+
+    for (String bank : banks) {
+      String best = maxJoltageForBankWithLength(bank);
+      long value = Long.parseLong(best);
+      total += value;
+    }
+
+    return total;
+  }
+
+  static int maxJoltageForBankTwoDigits(String bank) {
 
     int bestPair = 0;
     int bestTensSoFar = -1; // still no digit
@@ -80,6 +97,33 @@ public class E03_EscalatorBatteries implements Exercise {
     }
 
     return bestPair;
+  }
+
+  static String maxJoltageForBankWithLength(String bank) {
+
+    int n = bank.length();
+    int toRemove = n - E03_EscalatorBatteries.PART2_DIGITS;
+    StringBuilder stack = new StringBuilder(n); // stack
+
+    for (int i = 0; i < n; i++) {
+      char current = bank.charAt(i);
+
+      while (!stack.isEmpty()
+        && toRemove > 0 // still open to delete
+        && stack.charAt(stack.length() - 1) < current) { // last digit added to stack < current digit
+
+        stack.deleteCharAt(stack.length() - 1); // delete "blocking" digit
+        toRemove--;
+      }
+
+      stack.append(current);
+    }
+
+    if (stack.length() > E03_EscalatorBatteries.PART2_DIGITS) {
+      stack.setLength(E03_EscalatorBatteries.PART2_DIGITS);
+    }
+
+    return stack.toString();
   }
 
   public static void main(String[] args) {
